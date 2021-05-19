221e6d90f6a4b4ae72d1c4e9077fc1ff12748304
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-19 22:11:30 +0530

    add a SizedBox() widget in between the Container() and the Expanded() widget

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -22,6 +22,7 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
             ),
+            SizedBox(),
             Expanded(
               child: Column(
                 children: [
