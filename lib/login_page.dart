bf2c6063e9cccda7123874bdfd3d7411f5108af7
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-19 19:19:36 +0530

    remove the default constructor

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Container();
