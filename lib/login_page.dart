e832312a5ab3884951bda295c768f93e37e5cd65
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-19 21:00:39 +0530

    add a Row() widget as the child of the Column() widget

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -63,7 +63,8 @@ class LoginPage extends StatelessWidget {
                   ),
                 ),
               ),
-            )
+            ),
+            Row(),
           ],
         ),
       ),
