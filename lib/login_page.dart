9cee5a9f07883fc6fe4fe288a626b5929b856bb6
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-19 22:35:05 +0530

    Wrap the whole Column() widget along with its content inside a new widget Stack() as its child

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,107 +7,111 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
+        body: Stack(
           children: <Widget>[
-            Container(
-              alignment: Alignment.topLeft,
-              padding: EdgeInsets.only(left: 10.0),
-              child: Text(
-                'Welcome!',
-                style: TextStyle(
-                  color: Color(0xFF323232),
-                  fontSize: 50.0,
-                  fontFamily: 'NunitoSans',
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 100.0,
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.all(10.0),
-                    padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: 'Email address',
-                        hintStyle: TextStyle(
-                          color: Colors.grey[600],
-                          fontFamily: 'NunitoSans',
-                          fontWeight: FontWeight.w400,
-                        ),
-                        border: UnderlineInputBorder(
-                            borderRadius: BorderRadius.horizontal(
-                                left: Radius.circular(10.0),
-                                right: Radius.circular(10.0))),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(10.0),
-                    padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: 'Password',
-                        hintStyle: TextStyle(
-                          color: Colors.grey[600],
-                          fontFamily: 'NunitoSans',
-                          fontWeight: FontWeight.w400,
-                        ),
-                        border: UnderlineInputBorder(
-                            borderRadius: BorderRadius.horizontal(
-                                left: Radius.circular(10.0),
-                                right: Radius.circular(10.0))),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
+            Column(
               children: <Widget>[
                 Container(
-                  padding: EdgeInsets.all(10.0),
-                  margin: EdgeInsets.all(10.0),
+                  alignment: Alignment.topLeft,
+                  padding: EdgeInsets.only(left: 10.0),
                   child: Text(
-                    'Sign in',
+                    'Welcome!',
                     style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 25.0,
+                      color: Color(0xFF323232),
+                      fontSize: 50.0,
+                      fontFamily: 'NunitoSans',
                       fontWeight: FontWeight.bold,
-                      fontFamily: 'NotoSansJP',
                     ),
                   ),
                 ),
                 SizedBox(
-                  width: 200.0,
+                  height: 100.0,
                 ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return HomePage();
-                        },
+                Expanded(
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(10.0),
+                        padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: 'Email address',
+                            hintStyle: TextStyle(
+                              color: Colors.grey[600],
+                              fontFamily: 'NunitoSans',
+                              fontWeight: FontWeight.w400,
+                            ),
+                            border: UnderlineInputBorder(
+                                borderRadius: BorderRadius.horizontal(
+                                    left: Radius.circular(10.0),
+                                    right: Radius.circular(10.0))),
+                          ),
+                        ),
                       ),
-                    );
-                  },
-                  child: CircleAvatar(
-                    child: Icon(
-                      Icons.arrow_forward_ios,
-                      color: Colors.white,
-                    ),
-                    backgroundColor: Color(0xFFFA6400),
-                    radius: 40.0,
+                      Container(
+                        margin: EdgeInsets.all(10.0),
+                        padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: 'Password',
+                            hintStyle: TextStyle(
+                              color: Colors.grey[600],
+                              fontFamily: 'NunitoSans',
+                              fontWeight: FontWeight.w400,
+                            ),
+                            border: UnderlineInputBorder(
+                                borderRadius: BorderRadius.horizontal(
+                                    left: Radius.circular(10.0),
+                                    right: Radius.circular(10.0))),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(10.0),
+                      margin: EdgeInsets.all(10.0),
+                      child: Text(
+                        'Sign in',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'NotoSansJP',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 200.0,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return HomePage();
+                            },
+                          ),
+                        );
+                      },
+                      child: CircleAvatar(
+                        child: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.white,
+                        ),
+                        backgroundColor: Color(0xFFFA6400),
+                        radius: 40.0,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
