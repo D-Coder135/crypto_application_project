6a9d6629f1c289c7dc366ee32bc7379343ec34e9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-20 00:09:00 +0530

    Remove the default constructor from the class HomePage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
