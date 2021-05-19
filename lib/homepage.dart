ddad8367a909ba05ca26a90a0b383bbebc602902
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-20 00:11:01 +0530

    remove some code from the file homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,49 +4,6 @@ import 'package:flutter/widgets.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.only(bottom: 450.0),
-              color: Color(0xFF4037AB),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 40.0,
-                    backgroundImage: AssetImage('images/pic.png'),
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  Text(
-                    'My Portfolio',
-                    style: TextStyle(
-                        color: Color(0xFFACACAC),
-                        fontSize: 15.0,
-                        fontFamily: 'ArchitectsDaughter',
-                        fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  Text(
-                    '\$420,050',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 40.0,
-                        fontFamily: 'NotoSansJP',
-                        fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+    return Scaffold();
   }
 }
