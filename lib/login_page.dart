2e9cb2ba07263cdb05d96cd10f96258181efd526
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-19 22:35:56 +0530

    add one more Container() widget as the child of the Stack() widget along with the decoration property

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -9,6 +9,14 @@ class LoginPage extends StatelessWidget {
       child: Scaffold(
         body: Stack(
           children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/background.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
             Column(
               children: <Widget>[
                 Container(
