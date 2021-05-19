76ddfeed74b2d027e3cb7d4f0ee36fe3694ab80b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-19 22:10:54 +0530

    Remove the Expanded() widget

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -9,18 +9,16 @@ class LoginPage extends StatelessWidget {
       child: Scaffold(
         body: Column(
           children: <Widget>[
-            Expanded(
-              child: Container(
-                alignment: Alignment.topLeft,
-                padding: EdgeInsets.only(left: 10.0),
-                child: Text(
-                  'Welcome!',
-                  style: TextStyle(
-                    color: Color(0xFF323232),
-                    fontSize: 50.0,
-                    fontFamily: 'NunitoSans',
-                    fontWeight: FontWeight.bold,
-                  ),
+            Container(
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.only(left: 10.0),
+              child: Text(
+                'Welcome!',
+                style: TextStyle(
+                  color: Color(0xFF323232),
+                  fontSize: 50.0,
+                  fontFamily: 'NunitoSans',
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
