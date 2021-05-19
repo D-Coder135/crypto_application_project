4fda0444b21c39401e11002d05ae72b7b5c28648
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-19 22:39:22 +0530

    import the package flutter_signin_button.dart inside the file login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:demo_crypto_app/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginPage extends StatelessWidget {
   @override
