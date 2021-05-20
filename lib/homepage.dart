6a40c74788a3d558b35339287b76e86a6389eef3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-20 15:06:23 +0530

    add a custom color for the backgroundColor property for the Scaffold() widget

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,6 +4,8 @@ import 'package:flutter/widgets.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFD7E2F3),
+    );
   }
 }
