d81fdb89e90cd29b1b14ba52b773f63143b7d600
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-21 00:02:29 +0530

    add a method of type Future<dynamic> name getCryptoData()

--- lib/crypto_data.dart
+++ lib/crypto_data.dart
@@ -2,4 +2,6 @@ import 'package:demo_crypto_app/networking.dart';
 
 const coinCapURL = 'https://api.coincap.io/v2/rates';
 
-class CryptoData {}
+class CryptoData {
+  Future<dynamic> getCryptoData() {}
+}
