b91511063c2dbd2dea0881f807eb5602306e1403
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-20 15:33:51 +0530

    change the color for the Container() widget

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -19,7 +19,7 @@ class HomePage extends StatelessWidget {
               child: Container(
                 margin: EdgeInsets.all(15.0),
                 decoration: BoxDecoration(
-                    color: Colors.white,
+                    color: Color(0xFFFCE5C9),
                     borderRadius: BorderRadius.circular(10.0)),
               ),
             ),
