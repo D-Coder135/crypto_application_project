f1b1f549b08ecea60976ecbcba75fed85c785bae
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-21 00:03:27 +0530

    add the async keyword along with the method signature

--- lib/crypto_data.dart
+++ lib/crypto_data.dart
@@ -3,5 +3,5 @@ import 'package:demo_crypto_app/networking.dart';
 const coinCapURL = 'https://api.coincap.io/v2/rates';
 
 class CryptoData {
-  Future<dynamic> getCryptoData(var selectedCurrency) {}
+  Future<dynamic> getCryptoData(var selectedCurrency) async {}
 }
