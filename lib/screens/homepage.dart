aace995ed862bdf98d19cf7b4413d1edcfe71d84
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-20 20:45:28 +0530

    update the text inside the Text() widget

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -240,7 +240,7 @@ class HomePage extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text(
-                          'Bitcoin(BTC)',
+                          'Dogecoin(DOGE)',
                           style: TextStyle(
                             fontSize: 21.0,
                             fontWeight: FontWeight.bold,
