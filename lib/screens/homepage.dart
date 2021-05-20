a932c044f8c79d73cd2d41a8468f79e76be0c016
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-20 20:05:28 +0530

    paste the same Row() widget along with its contents as the child of all the Container() widgets

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -70,32 +70,208 @@ class HomePage extends StatelessWidget {
               child: Container(
                 margin: EdgeInsets.all(15.0),
                 decoration: BoxDecoration(
-                    color: Colors.lightBlueAccent[100],
-                    borderRadius: BorderRadius.circular(10.0)),
+                  color: Colors.lightBlueAccent[100],
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: AssetImage('images/Bitcoin.png'),
+                      radius: 40.0,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Bitcoin(BTC)',
+                          style: TextStyle(
+                            fontSize: 21.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Farro',
+                            letterSpacing: 2.0,
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          margin: EdgeInsets.only(top: 5.0, right: 60.0),
+                          child: Text(
+                            '\$45324',
+                            style: TextStyle(
+                              fontSize: 17.0,
+                              color: Color(0xFFF29726),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Rs 123565',
+                      style: TextStyle(
+                        fontSize: 19.0,
+                        fontWeight: FontWeight.w900,
+                        color: Color(0xFFF29726),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Expanded(
               child: Container(
                 margin: EdgeInsets.all(15.0),
                 decoration: BoxDecoration(
-                    color: Colors.lightGreen[200],
-                    borderRadius: BorderRadius.circular(10.0)),
+                  color: Colors.lightGreen[200],
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: AssetImage('images/Bitcoin.png'),
+                      radius: 40.0,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Bitcoin(BTC)',
+                          style: TextStyle(
+                            fontSize: 21.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Farro',
+                            letterSpacing: 2.0,
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          margin: EdgeInsets.only(top: 5.0, right: 60.0),
+                          child: Text(
+                            '\$45324',
+                            style: TextStyle(
+                              fontSize: 17.0,
+                              color: Color(0xFFF29726),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Rs 123565',
+                      style: TextStyle(
+                        fontSize: 19.0,
+                        fontWeight: FontWeight.w900,
+                        color: Color(0xFFF29726),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Expanded(
               child: Container(
                 margin: EdgeInsets.all(15.0),
                 decoration: BoxDecoration(
-                    color: Colors.purple[200],
-                    borderRadius: BorderRadius.circular(10.0)),
+                  color: Colors.purple[200],
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: AssetImage('images/Bitcoin.png'),
+                      radius: 40.0,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Bitcoin(BTC)',
+                          style: TextStyle(
+                            fontSize: 21.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Farro',
+                            letterSpacing: 2.0,
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          margin: EdgeInsets.only(top: 5.0, right: 60.0),
+                          child: Text(
+                            '\$45324',
+                            style: TextStyle(
+                              fontSize: 17.0,
+                              color: Color(0xFFF29726),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Rs 123565',
+                      style: TextStyle(
+                        fontSize: 19.0,
+                        fontWeight: FontWeight.w900,
+                        color: Color(0xFFF29726),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Expanded(
               child: Container(
                 margin: EdgeInsets.all(15.0),
                 decoration: BoxDecoration(
-                    color: Color(0xFFF4D96B),
-                    borderRadius: BorderRadius.circular(10.0)),
+                  color: Color(0xFFF4D96B),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: AssetImage('images/Bitcoin.png'),
+                      radius: 40.0,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Bitcoin(BTC)',
+                          style: TextStyle(
+                            fontSize: 21.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Farro',
+                            letterSpacing: 2.0,
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          margin: EdgeInsets.only(top: 5.0, right: 60.0),
+                          child: Text(
+                            '\$45324',
+                            style: TextStyle(
+                              fontSize: 17.0,
+                              color: Color(0xFFF29726),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Rs 123565',
+                      style: TextStyle(
+                        fontSize: 19.0,
+                        fontWeight: FontWeight.w900,
+                        color: Color(0xFFF29726),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
