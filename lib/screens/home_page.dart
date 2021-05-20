c039dfa7408a551d94879c6ec7710f391bcb4159
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-21 00:15:41 +0530

    make the HomePage class extends the StatefulWidget instead of StatelessWidget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
