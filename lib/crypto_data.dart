b226df796f151d528c94962d49e9864fdb210ef0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-21 00:06:55 +0530

    add an object of the class NetworkHelper() and pass the const field url and the parameter selectedCurrency to it as an argument inside the body of the method getCryptoData()

--- lib/crypto_data.dart
+++ lib/crypto_data.dart
@@ -3,5 +3,8 @@ import 'package:demo_crypto_app/networking.dart';
 const coinCapURL = 'https://api.coincap.io/v2/rates';
 
 class CryptoData {
-  Future<dynamic> getCryptoData(var selectedCurrency) async {}
+  Future<dynamic> getCryptoData(var selectedCurrency) async {
+    NetworkHelper networkHelper =
+        NetworkHelper('$coinCapURL/$selectedCurrency');
+  }
 }
