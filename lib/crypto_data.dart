fbaf5917e0010edb12fbc67f5bec03ff5f474f12
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-21 20:12:43 +0530

    set the list cryptoCurrenciesList as cost field

--- lib/crypto_data.dart
+++ lib/crypto_data.dart
@@ -2,7 +2,7 @@ import 'package:demo_crypto_app/networking.dart';
 
 const coinCapURL = 'https://api.coincap.io/v2/rates';
 
-List<String> cryptoCurrenciesList = [];
+const List<String> cryptoCurrenciesList = [];
 
 class CryptoData {
   Future<dynamic> getCryptoData(var selectedCurrency) async {
