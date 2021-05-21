0c1ef10e0643fa125c8bd19a7ea66c29a7190f28
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-21 20:12:05 +0530

    initialize a new list of type List<String> inside the file crypto_data.dart

--- lib/crypto_data.dart
+++ lib/crypto_data.dart
@@ -2,6 +2,8 @@ import 'package:demo_crypto_app/networking.dart';
 
 const coinCapURL = 'https://api.coincap.io/v2/rates';
 
+List<String> cryptoCurrenciesList = [];
+
 class CryptoData {
   Future<dynamic> getCryptoData(var selectedCurrency) async {
     NetworkHelper networkHelper =
