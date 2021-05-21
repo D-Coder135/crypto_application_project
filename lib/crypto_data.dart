16b34448c13d989f12fe5b47488c7d0b9fdbc532
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-21 20:22:10 +0530

    add some string values inside the list of cryptoCurrenciesList

--- lib/crypto_data.dart
+++ lib/crypto_data.dart
@@ -2,7 +2,13 @@ import 'package:demo_crypto_app/networking.dart';
 
 const coinCapURL = 'https://api.coincap.io/v2/rates';
 
-const List<String> cryptoCurrenciesList = [];
+const List<String> cryptoCurrenciesList = [
+  'bitcoin',
+  'ethereum',
+  'dogecoin',
+  'litecoin',
+  'dash',
+];
 
 class CryptoData {
   Future<dynamic> getCryptoData(var selectedCurrency) async {
