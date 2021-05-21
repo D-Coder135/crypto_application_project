4cde94346b4b1a833090562904254032600e51d2
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-21 23:43:40 +0530

    add a new file reusable_card.dart inside which add a class ReusableCard inside which extends StatelessWidget and call the class inside the file home_page.dart

---
+++ lib/reusable_card.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+  // const ReusbaleCard({Key key}) : super(key: key);
+
+  final Color colour;
+  final String imageFileName;
+  final String text;
+  final double letterSpacingValue;
+  final double marginValue;
+  final String textForUSDRate;
+  final Color colorForTextUSD;
+  final String textForINRRate;
+  final Color colorForTextINR;
+
+  ReusableCard(
+      this.colour,
+      this.imageFileName,
+      this.text,
+      this.letterSpacingValue,
+      this.marginValue,
+      this.textForUSDRate,
+      this.colorForTextUSD,
+      this.textForINRRate,
+      this.colorForTextINR);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+          color: colour, borderRadius: BorderRadius.circular(10.0)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          CircleAvatar(
+            backgroundImage: AssetImage(imageFileName),
+            radius: 40.0,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                text,
+                style: TextStyle(
+                  fontSize: 21.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Farro',
+                  letterSpacing: letterSpacingValue,
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(top: 5.0, right: marginValue),
+                child: Text(
+                  textForUSDRate,
+                  style: TextStyle(
+                    fontSize: 17.0,
+                    color: colorForTextUSD,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Text(
+            textForINRRate,
+            style: TextStyle(
+              fontSize: 19.0,
+              fontWeight: FontWeight.w900,
+              color: colorForTextINR,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:demo_crypto_app/reusable_card.dart';
 import 'package:demo_crypto_app/services/crypto_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -42,54 +43,16 @@ class _HomePageState extends State<HomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Expanded(
-              child: Container(
-                margin: EdgeInsets.all(20.0),
-                decoration: BoxDecoration(
-                    color: Color(0xFFFCE5C9),
-                    borderRadius: BorderRadius.circular(10.0)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundImage: AssetImage('images/Bitcoin.png'),
-                      radius: 40.0,
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          'Bitcoin(BTC)',
-                          style: TextStyle(
-                            fontSize: 21.0,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Farro',
-                            letterSpacing: 2.0,
-                          ),
-                        ),
-                        Container(
-                          alignment: Alignment.centerLeft,
-                          margin: EdgeInsets.only(top: 5.0, right: 80.0),
-                          child: Text(
-                            '\$ ${currentRateInUSD[0]}',
-                            style: TextStyle(
-                              fontSize: 17.0,
-                              color: Color(0xFFF29726),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      '₹ ${currentRateInINR[0]}',
-                      style: TextStyle(
-                        fontSize: 19.0,
-                        fontWeight: FontWeight.w900,
-                        color: Color(0xFFF29726),
-                      ),
-                    ),
-                  ],
-                ),
+              child: ReusableCard(
+                Color(0xFFFCE5C9),
+                'images/Bitcoin.png',
+                'Bitcoin(BTC)',
+                2.0,
+                80.0,
+                '\$ ${currentRateInUSD[0]}',
+                Color(0xFFF29726),
+                '₹ ${currentRateInINR[0]}',
+                Color(0xFFF29726),
               ),
             ),
             Expanded(
