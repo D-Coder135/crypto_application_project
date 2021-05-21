7c6a376fe9d20d747086541faf488459a7ded64f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-21 23:54:16 +0530

    Remove out some comments from the code

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -93,56 +93,6 @@ class _HomePageState extends State<HomePage> {
                 '₹ ${currentRateInINR[3]}',
                 Colors.purple[800],
               ),
-              // child: Container(
-              //   margin: EdgeInsets.all(20.0),
-              //   decoration: BoxDecoration(
-              //     color: Colors.purple[200],
-              //     borderRadius: BorderRadius.circular(10.0),
-              //   ),
-              //   child: Row(
-              //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              //     children: <Widget>[
-              //       CircleAvatar(
-              //         backgroundImage: AssetImage('images/Litecoin.png'),
-              //         radius: 40.0,
-              //       ),
-              //       Column(
-              //         mainAxisAlignment: MainAxisAlignment.center,
-              //         children: <Widget>[
-              //           Text(
-              //             'Litecoin(LTC)',
-              //             style: TextStyle(
-              //               fontSize: 21.0,
-              //               fontWeight: FontWeight.bold,
-              //               fontFamily: 'Farro',
-              //               letterSpacing: 1.0,
-              //             ),
-              //           ),
-              //           Container(
-              //             alignment: Alignment.centerLeft,
-              //             margin: EdgeInsets.only(top: 5.0, right: 90.0),
-              //             child: Text(
-              //               '\$ ${currentRateInUSD[3]}',
-              //               style: TextStyle(
-              //                 fontSize: 17.0,
-              //                 color: Colors.purple[700],
-              //                 fontWeight: FontWeight.bold,
-              //               ),
-              //             ),
-              //           ),
-              //         ],
-              //       ),
-              //       Text(
-              //         '₹ ${currentRateInINR[3]}',
-              //         style: TextStyle(
-              //           fontSize: 19.0,
-              //           fontWeight: FontWeight.w900,
-              //           color: Colors.purple[800],
-              //         ),
-              //       ),
-              //     ],
-              //   ),
-              // ),
             ),
             Expanded(
               child: Container(
