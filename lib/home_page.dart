bb1e105f25fae71be8e04e43b93fc19080c73bf5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-23 10:05:14 +0530

    remove the default constructor

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key key}) : super(key: key);
-
   @override
   _HomePageState createState() => _HomePageState();
 }
