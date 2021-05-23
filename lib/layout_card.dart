3974e5fcacf552ca384a557b6827082f4f380b98
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-23 11:59:54 +0530

    add a constructor inside which initialize the final field

--- lib/layout_card.dart
+++ lib/layout_card.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class LayoutCard extends StatelessWidget {
   final Widget childWidget;
 
+  LayoutCard(this.childWidget);
+
   @override
   Widget build(BuildContext context) {
     return Container();
