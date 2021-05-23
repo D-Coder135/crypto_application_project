bce06872c83131f9c3570985adc13373936ada7c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-23 10:01:14 +0530

    refactor a file name

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,4 +1,4 @@
-import 'package:demo_crypto_app/screens/crypto_list.dart';
+import 'package:demo_crypto_app/screens/crypto_list_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
