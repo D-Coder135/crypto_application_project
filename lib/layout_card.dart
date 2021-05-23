df81fc7249111190af72f6fc74937b95feb43cca
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-23 11:59:33 +0530

    add a final field of type Widget

--- lib/layout_card.dart
+++ lib/layout_card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class LayoutCard extends StatelessWidget {
+  final Widget childWidget;
+
   @override
   Widget build(BuildContext context) {
     return Container();
