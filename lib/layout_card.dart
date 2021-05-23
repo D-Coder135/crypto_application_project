3cfef4bcc1b7db9a8189030d4a7dfed1ca7e5777
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-23 11:58:53 +0530

    remove the default constructor

--- lib/layout_card.dart
+++ lib/layout_card.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class LayoutCard extends StatelessWidget {
-  const LayoutCard({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Container();
