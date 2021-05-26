23699c10d520c5575b5c07151427d16d89fdd096
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 12:47:08 +0530

    return a Scaffold() widget inside the build() method

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -9,6 +9,6 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
