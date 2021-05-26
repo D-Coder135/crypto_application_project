5c4d63197d9c86fa274c9a3e59748ba7a4ccb9cd
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 11:51:51 +0530

    add a static const field of type String inside the class RegistrationScreen

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class RegistrationScreen extends StatefulWidget {
+  static const String id = 'Registration_Screen';
+
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
