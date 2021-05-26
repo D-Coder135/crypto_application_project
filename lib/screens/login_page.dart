634df4eb509b2ef487aec3efa006cdfbe0444051
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 12:50:58 +0530

    add a TextField() widget along with some contents inside it as the child of the Column() widget

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -26,6 +26,36 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(
               height: 48.0,
             ),
+            TextField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[350],
+                hintText: 'Enter your email',
+                hintStyle: TextStyle(
+                  color: Colors.grey,
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+              ),
+            ),
           ],
         ),
       ),
