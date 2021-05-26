40cab1d09c4ee3d3742f52cae7f4e9139004564f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 11:40:33 +0530

    add Padding() widgets along with some contents as the child of the Column() widget

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -167,6 +167,44 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 48.0,
                   ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                    child: Material(
+                      elevation: 5.0,
+                      color: Colors.lightBlueAccent,
+                      borderRadius: BorderRadius.circular(30.0),
+                      child: MaterialButton(
+                        onPressed: () {
+                          //Go to login screen.
+                          Navigator.pushNamed(context, LoginScreen.id);
+                        },
+                        minWidth: 200.0,
+                        height: 42.0,
+                        child: Text(
+                          'Log In',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                    child: Material(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(30.0),
+                      elevation: 5.0,
+                      child: MaterialButton(
+                        onPressed: () {
+                          //Go to registration screen.
+                          Navigator.pushNamed(context, RegistrationScreen.id);
+                        },
+                        minWidth: 200.0,
+                        height: 42.0,
+                        child: Text(
+                          'Register',
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
