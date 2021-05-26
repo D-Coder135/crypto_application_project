3f881f8ed509303ac225cce496e8794fc82e8770
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 11:57:44 +0530

    add a TextField() along with some contents inside it as the child of the Column() widget

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -27,6 +27,35 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             SizedBox(
               height: 48.0,
             ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                //Do something with the user input.
+                // email = value;
+              },
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[350],
+                hintText: 'Enter your email',
+                hintStyle: TextStyle(
+                  color: Colors.grey,
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+              ),
+            ),
           ],
         ),
       ),
