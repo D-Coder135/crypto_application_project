95ec5a191322838ac9f579d632a6b0eb34cbe29b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 11:50:47 +0530

    remove the default constructor

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class RegistrationScreen extends StatefulWidget {
-  const RegistrationScreen({Key key}) : super(key: key);
-
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
