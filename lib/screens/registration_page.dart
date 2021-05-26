0137f874a7440d06d442435f3cd88a54fd0dd014
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 11:50:33 +0530

    add a class RegistrationScreen inside which extends StatefulWidget

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key key}) : super(key: key);
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
