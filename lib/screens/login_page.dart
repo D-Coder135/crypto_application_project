eb491c69250b2388baa90586f3ecc652c7920d74
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 11:39:02 +0530

    add a SizedBox() widget as the child of the Column() widget

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -164,6 +164,7 @@ class LoginPage extends StatelessWidget {
                       ),
                     ],
                   ),
+                  SizedBox(),
                 ],
               ),
             ),
