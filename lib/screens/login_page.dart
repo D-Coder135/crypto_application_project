323dbd7c8eee1a24f66f5bd5c49c6c1dd40e8641
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 11:37:05 +0530

    import the animated_text_kit.dart file inside the file login_page.dart

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import '../home_page.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class LoginPage extends StatelessWidget {
   @override
