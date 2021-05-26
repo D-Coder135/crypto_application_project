796ce11b3e2c4be28ef36593c31ac36bf348eedb
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 12:47:45 +0530

    add a Padding() widget inside the body of the Scaffold() widget

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -9,6 +9,8 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(),
+    );
   }
 }
