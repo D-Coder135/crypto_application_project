81a97391a64eb4a06d1ab0df59e1cfbc0894d544
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 12:53:49 +0530

    add a Padding() widget along with some contents inside it as the child of the Column() widget

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -82,6 +82,24 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(
               height: 24.0,
             ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                elevation: 5.0,
+                child: MaterialButton(
+                  onPressed: () {
+                    //Implement login functionality.
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Log In',
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
