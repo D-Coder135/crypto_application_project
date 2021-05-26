a23081eddc06b8d1fa49b742376984f2a6de8171
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 11:52:19 +0530

    return the Scaffold() widget

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -10,6 +10,6 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
