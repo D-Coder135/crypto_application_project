f490436ca1c32b73d19ca532e66bac3175efb3f7
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 12:36:56 +0530

    add one more route for the HomePage() class inside the routes property

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demo_crypto_app/home_page.dart';
 import 'package:demo_crypto_app/screens/crypto_list_page.dart';
 import 'package:demo_crypto_app/screens/registration_page.dart';
 import 'package:demo_crypto_app/screens/welcome_page.dart';
@@ -15,6 +16,7 @@ class CryptoApp extends StatelessWidget {
         FrontPage.id: (context) => FrontPage(),
         WelcomePage.id: (context) => WelcomePage(),
         RegistrationScreen.id: (context) => RegistrationScreen(),
+        HomePage.id: (context) => HomePage(),
         CryptoList.id: (context) => CryptoList(),
       },
     );
