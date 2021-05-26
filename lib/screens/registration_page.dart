2db846a9780c74dd7e3d5c18df78f47535972bed
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 12:38:19 +0530

    remove unused import statement

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -1,8 +1,6 @@
 import 'package:demo_crypto_app/home_page.dart';
 import 'package:flutter/material.dart';
 
-import 'crypto_list_page.dart';
-
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'Registration_Screen';
 
