fc1259e899e5af892cea231aa38f3c1d39ba4866
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 12:00:09 +0530

    add another TextField() widget along with some contents inside it

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -59,6 +59,35 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             SizedBox(
               height: 8.0,
             ),
+            TextField(
+              obscureText: true,
+              textAlign: TextAlign.center,
+              onChanged: (value) {
+                //Do something with the user input.
+                password = value;
+              },
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[350],
+                hintText: 'Enter your password',
+                hintStyle: TextStyle(
+                  color: Colors.grey,
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+              ),
+            ),
           ],
         ),
       ),
