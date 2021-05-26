9d2d427f74d58e2ca8749de875d29c3c61679c89
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-26 12:46:20 +0530

    add one more route for the LoginPage() class inside the routes property

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:demo_crypto_app/screens/home_page.dart';
 import 'package:demo_crypto_app/screens/crypto_list_page.dart';
+import 'package:demo_crypto_app/screens/login_page.dart';
 import 'package:demo_crypto_app/screens/registration_page.dart';
 import 'package:demo_crypto_app/screens/welcome_page.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +16,7 @@ class CryptoApp extends StatelessWidget {
       routes: {
         FrontPage.id: (context) => FrontPage(),
         WelcomePage.id: (context) => WelcomePage(),
+        LoginPage.id: (context) => LoginPage(),
         RegistrationScreen.id: (context) => RegistrationScreen(),
         HomePage.id: (context) => HomePage(),
         CryptoList.id: (context) => CryptoList(),
