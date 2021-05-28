d904af5eb5a660daaca63178de8848202a198eb9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-28 23:34:27 +0530

    create a class name WelcomePage inside which extends the StatefulWidget

--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -3,3 +3,17 @@ import 'package:demo_crypto_app/screens/registration_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key key}) : super(key: key);
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
