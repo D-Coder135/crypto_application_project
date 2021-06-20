71273ff7a228fc714b3c1d78a03d6073d9c609d3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-20 10:32:41 +0530

    remove the pre-written constructor from the class CryptoList

--- lib/services/crypto_list.dart
+++ lib/services/crypto_list.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class CryptoList extends StatelessWidget {
-  const CryptoList({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Container();
