3f860b340ed30a5d9c082670c04a1b57dade8145
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-20 10:32:09 +0530

    add a class name CryptoList inside the file crypto_list.dart inside which extends StatelessWidget

--- lib/services/crypto_list.dart
+++ lib/services/crypto_list.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class CryptoList extends StatelessWidget {
+  const CryptoList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
