385545549eed52c82571190837180d7af2b659d0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-21 12:10:44 +0530

    remove the SafeArea() widget from the file front_page.dart

--- lib/screens/front_page.dart
+++ lib/screens/front_page.dart
@@ -8,83 +8,81 @@ class FrontPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: Hero(
-                tag: 'logo',
-                child: Container(
-                  child: Image(
-                    image: AssetImage('images/bitcoin.png'),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Hero(
+              tag: 'logo',
               child: Container(
-                padding: EdgeInsets.all(10.0),
-                margin: EdgeInsets.only(left: 40.0, right: 40.0),
-                child: Column(
-                  children: [
-                    AnimatedTextKit(
-                      animatedTexts: [
-                        TypewriterAnimatedText(
-                          'Plan for the future from now on!',
-                          textAlign: TextAlign.center,
-                          textStyle: TextStyle(
-                            color: Color(0xFF262163),
-                            fontSize: 30.0,
-                            fontFamily: 'NotoSansJP',
-                            fontWeight: FontWeight.bold,
-                          ),
-                          speed: Duration(milliseconds: 150),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                    Text(
-                      'We provide guidance regarding stock and crypto currencies investing.',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color(0xFFACACAC),
-                        fontSize: 15.0,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 60.0,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return WelcomePage();
-                            },
-                          ),
-                        );
-                      },
-                      child: CircleAvatar(
-                        child: Icon(
-                          Icons.arrow_forward_ios,
-                          color: Colors.white,
-                        ),
-                        backgroundColor: Color(0xFFF87B59),
-                        radius: 40.0,
-                      ),
-                    ),
-                  ],
+                child: Image(
+                  image: AssetImage('images/bitcoin.png'),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.only(left: 40.0, right: 40.0),
+              child: Column(
+                children: [
+                  AnimatedTextKit(
+                    animatedTexts: [
+                      TypewriterAnimatedText(
+                        'Plan for the future from now on!',
+                        textAlign: TextAlign.center,
+                        textStyle: TextStyle(
+                          color: Color(0xFF262163),
+                          fontSize: 30.0,
+                          fontFamily: 'NotoSansJP',
+                          fontWeight: FontWeight.bold,
+                        ),
+                        speed: Duration(milliseconds: 150),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Text(
+                    'We provide guidance regarding stock and crypto currencies investing.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xFFACACAC),
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 60.0,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return WelcomePage();
+                          },
+                        ),
+                      );
+                    },
+                    child: CircleAvatar(
+                      child: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white,
+                      ),
+                      backgroundColor: Color(0xFFF87B59),
+                      radius: 40.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
