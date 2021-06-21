a342d5488501be57fe601dbfe631e13171eee487
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-21 14:56:39 +0530

    add a ReusableCard() widget as the child of the ScaleTransition() widget

--- lib/screens/crypto_list_page.dart
+++ lib/screens/crypto_list_page.dart
@@ -199,6 +199,7 @@ class _CryptoListState extends State<CryptoList>
           ),
           ScaleTransition(
             scale: animation,
+            child: ReusableCard(),
           )
         ],
       ),
