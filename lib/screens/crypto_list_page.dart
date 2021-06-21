6166980b0123241323fa28d317fe66c3fc1879fd
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-21 14:34:58 +0530

    remove the Expanded() widget

--- lib/screens/crypto_list_page.dart
+++ lib/screens/crypto_list_page.dart
@@ -143,18 +143,16 @@ class _CryptoListState extends State<CryptoList>
           ),
           ScaleTransition(
             scale: animation,
-            child: Expanded(
-              child: ReusableCard(
-                Color(0xFFF4D96B),
-                'images/usdcoin.jpg',
-                'USDCoin(USDC)',
-                2.5,
-                150.0,
-                '\$ $currentRateInUSDUSDC',
-                Colors.orange[700],
-                '₹ $currentRateInINRUSDC',
-                Colors.orange[800],
-              ),
+            child: ReusableCard(
+              Color(0xFFF4D96B),
+              'images/usdcoin.jpg',
+              'USDCoin(USDC)',
+              2.5,
+              150.0,
+              '\$ $currentRateInUSDUSDC',
+              Colors.orange[700],
+              '₹ $currentRateInINRUSDC',
+              Colors.orange[800],
             ),
           ),
           ScaleTransition(
