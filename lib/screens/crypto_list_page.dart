4ec5abe141497f5f2e683e10d3b9063f05d0e35b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-21 11:33:50 +0530

    override the dispose() function inside the class _CryptoListState

--- lib/screens/crypto_list_page.dart
+++ lib/screens/crypto_list_page.dart
@@ -90,6 +90,12 @@ class _CryptoListState extends State<CryptoList>
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
