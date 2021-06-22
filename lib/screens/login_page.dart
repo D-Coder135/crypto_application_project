058ad29acdd1afad8422a19db02318bdd2b23b1c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-23 00:20:49 +0530

    move the Navigator.pushNamed() method call inside the body of the if() block

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -99,8 +99,9 @@ class _LoginPageState extends State<LoginPage> {
                     try {
                       final loggedInUser = _auth.signInWithEmailAndPassword(
                           email: email, password: password);
-                      if (loggedInUser != null) {}
-                      Navigator.pushNamed(context, HomePage.id);
+                      if (loggedInUser != null) {
+                        Navigator.pushNamed(context, HomePage.id);
+                      }
                     } catch (exception) {
                       print(exception);
                     }
