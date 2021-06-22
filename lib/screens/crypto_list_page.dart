3269d09391e3892e738f9d64ea56987a91a3f1ca
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-22 11:05:15 +0530

    add the await keyword along with the method call

--- lib/screens/crypto_list_page.dart
+++ lib/screens/crypto_list_page.dart
@@ -73,7 +73,7 @@ class _CryptoListState extends State<CryptoList>
   // }
   void getData() async {
     CryptoData data = CryptoData();
-    data.getCryptoData();
+    await data.getCryptoData();
   }
 
   @override
