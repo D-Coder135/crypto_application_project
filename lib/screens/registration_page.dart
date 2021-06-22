b35f68101e433f2c4c79626040e7f5645fa158e0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-22 23:16:00 +0530

    add a String field name email inside the class _RegistrationScreenState

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -9,6 +9,8 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String email;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
