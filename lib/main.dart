8bc8dff757b0505a1dd71e1e4cd65da5f1d881b3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-22 23:45:48 +0530

    add the async keyword to the method signature of the main() method

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/front_page.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp();
   runApp(CryptoApp());
