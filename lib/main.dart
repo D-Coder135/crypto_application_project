da69ab2332722704aef04788a5003fa80844ed32
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-22 23:45:20 +0530

    call the Firebase.initializeApp() method inside the main() method body

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:demo_crypto_app/screens/crypto_list_page.dart';
 import 'package:demo_crypto_app/screens/login_page.dart';
 import 'package:demo_crypto_app/screens/registration_page.dart';
 import 'package:demo_crypto_app/screens/welcome_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/front_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(CryptoApp());
 }
 
