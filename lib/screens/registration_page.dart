46e25d63070dc077c80630284e46904a6621ac76
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-22 23:22:10 +0530

    initialize a new private final field name auth with value FirebaseAuth.instance

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -10,6 +10,7 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
   String email;
   String password;
 
