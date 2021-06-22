5698cc9f530084a8132f641bbbf1fbd88fb2d2be
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-23 00:28:38 +0530

    initialize a field of type bool name isLoading inside the class _RegistrationScreenState

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -14,6 +14,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
   String email;
   String password;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
