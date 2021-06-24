2376519d97fae9e605a4e06751f1e78156c3b7b7
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-24 22:23:44 +0530

    add the fontWeight property for the TextStyle() widget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -54,6 +54,7 @@ class _HomePageState extends State<HomePage> {
                   color: Colors.white,
                   fontSize: 30.0,
                   fontFamily: 'Marker',
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
