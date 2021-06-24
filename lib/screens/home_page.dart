dd4a57ca84b8de686b27075064b3a70a2b1407e0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-24 22:33:25 +0530

    add the textStyle property for the TyperAnimatedText() widget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -53,6 +53,7 @@ class _HomePageState extends State<HomePage> {
                 animatedTexts: [
                   TyperAnimatedText(
                     "Subscribed Currencies!",
+                    textStyle:
                   ),
                 ],
               ),
