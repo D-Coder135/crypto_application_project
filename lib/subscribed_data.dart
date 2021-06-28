7ad186275fc8285bd15c3e60fc3a2ea935691a71
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-28 11:39:06 +0530

    add a demo object of the class SubscribedWidget inside the list _subscribedCurrencies

--- lib/subscribed_data.dart
+++ lib/subscribed_data.dart
@@ -2,5 +2,7 @@ import 'package:demo_crypto_app/subscribed_widget.dart';
 import 'package:flutter/material.dart';
 
 class SubscribedData extends ChangeNotifier {
-  List<SubscribedWidget> _subscribedCurrencies = [];
+  List<SubscribedWidget> _subscribedCurrencies = [
+    SubscribedWidget('BitCoin', 'images/Bitcoin.png', '54000', '2654659')
+  ];
 }
