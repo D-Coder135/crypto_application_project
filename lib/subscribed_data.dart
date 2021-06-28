a22715aebe69279ace84ba93bb0e3ae7ad3519f9
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-29 00:08:03 +0530

    empty the list _subscribedCurrencies

--- lib/subscribed_data.dart
+++ lib/subscribed_data.dart
@@ -2,9 +2,7 @@ import 'package:demo_crypto_app/currency.dart';
 import 'package:flutter/material.dart';
 
 class SubscribedData extends ChangeNotifier {
-  List<Currency> _subscribedCurrencies = [
-    Currency('BitCoin', 'images/Bitcoin.png', 54000, 2654659)
-  ];
+  List<Currency> _subscribedCurrencies = [];
 
   List<Currency> get subscribedCurrencies => _subscribedCurrencies;
 
