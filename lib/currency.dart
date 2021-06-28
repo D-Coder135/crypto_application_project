d843f4f3acc0a53063462dabf0417c35ad6ee9b5
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-29 00:06:44 +0530

    update the type for the final fields inside the class Currency

--- lib/currency.dart
+++ lib/currency.dart
@@ -1,8 +1,8 @@
 class Currency {
   final String currencyName;
   final String imageFile;
-  final int rateInUSD;
-  final int rateInINR;
+  final String rateInUSD;
+  final String rateInINR;
 
   Currency(this.currencyName, this.imageFile, this.rateInUSD, this.rateInINR);
 }
