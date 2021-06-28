cab039b31d2fbe6f5779bbb8ec940ea958ee6df1
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-28 11:11:05 +0530

    remove the pre-written constructor inside the class SubscribedList

--- lib/subscribed_list.dart
+++ lib/subscribed_list.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class SubscribedList extends StatelessWidget {
-  const SubscribedList({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Container();
