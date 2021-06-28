6f67ea2976342fa993c0eaad5756cfd763881180
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-28 11:10:39 +0530

    add a class SubscribedList inside which extends the StatelessWidget

--- lib/subscribed_list.dart
+++ lib/subscribed_list.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class SubscribedList extends StatelessWidget {
+  const SubscribedList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
