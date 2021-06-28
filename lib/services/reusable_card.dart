00cbd187df3d0bda6c3641c811908100667b9522
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-28 23:34:03 +0530

    add a TextButton() widget as the child of the Row() widget

--- lib/services/reusable_card.dart
+++ lib/services/reusable_card.dart
@@ -81,6 +81,7 @@ class ReusableCard extends StatelessWidget {
                 color: colorForTextINR,
               ),
             ),
+            TextButton(),
           ],
         ),
       ),
