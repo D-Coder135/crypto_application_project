171574a12d1464dcae9141e8b7aaeca5916fe2ec
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-28 11:35:44 +0530

    initialize an empty list of type SubscribedWidget name subscribedCurrencies

--- lib/subscribed_data.dart
+++ lib/subscribed_data.dart
@@ -1,3 +1,6 @@
+import 'package:demo_crypto_app/subscribed_widget.dart';
 import 'package:flutter/material.dart';
 
-class SubscribedData extends ChangeNotifier {}
+class SubscribedData extends ChangeNotifier {
+  List<SubscribedWidget> _subscribedCurrencies = [];
+}
