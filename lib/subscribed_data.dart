0521ff36968791311f8d9b00f5a6bf427c800b5f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-28 14:20:52 +0530

    update the type for the list inside the class SubscribedData

--- lib/subscribed_data.dart
+++ lib/subscribed_data.dart
@@ -1,9 +1,10 @@
+import 'package:demo_crypto_app/currency.dart';
 import 'package:demo_crypto_app/subscribed_widget.dart';
 import 'package:flutter/material.dart';
 
 class SubscribedData extends ChangeNotifier {
-  List<SubscribedWidget> _subscribedCurrencies = [
-    SubscribedWidget('BitCoin', 'images/Bitcoin.png', 54000, 2654659)
+  List<Currency> _subscribedCurrencies = [
+    Currency('BitCoin', 'images/Bitcoin.png', 54000, 2654659)
   ];
 
   List<SubscribedWidget> get currencies => _subscribedCurrencies;
