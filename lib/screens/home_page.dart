244d7b9b3d61ae81b8117ba8bf7b27f2056e1393
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-17 14:07:43 +0530

    wrap the Column() widget inside the Stack() widget as its child

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -30,48 +30,52 @@ class _HomePageState extends State<HomePage> {
           Navigator.pushNamed(context, CryptoList.id);
         },
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(
-              top: 60.0,
-              left: 30.0,
-              right: 30.0,
-              bottom: 30.0,
-            ),
-            child: Padding(
-              padding: EdgeInsets.only(top: 130.0, bottom: 100.0),
-              child: AnimatedTextKit(
-                isRepeatingAnimation: false,
-                animatedTexts: [
-                  TyperAnimatedText(
-                    "Subscribed Currencies!",
-                    textStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30.0,
-                      fontFamily: 'Marker',
-                      fontWeight: FontWeight.w500,
-                    ),
-                    speed: Duration(
-                      milliseconds: 90,
-                    ),
+      body: Stack(
+        children: [
+          Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(
+                  top: 60.0,
+                  left: 30.0,
+                  right: 30.0,
+                  bottom: 30.0,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(top: 130.0, bottom: 100.0),
+                  child: AnimatedTextKit(
+                    isRepeatingAnimation: false,
+                    animatedTexts: [
+                      TyperAnimatedText(
+                        "Subscribed Currencies!",
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                          fontFamily: 'Marker',
+                          fontWeight: FontWeight.w500,
+                        ),
+                        speed: Duration(
+                          milliseconds: 90,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20.0),
-                  topRight: Radius.circular(20.0),
                 ),
               ),
-              child: SubscribedList(),
-            ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    ),
+                  ),
+                  child: SubscribedList(),
+                ),
+              ),
+            ],
           ),
         ],
       ),
