1fe058a069f266fe86de60682fe87f7897e54065
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-17 14:04:23 +0530

    delete the file currency.dart from the project

--- lib/currency.dart
+++
@@ -1,8 +0,0 @@
-class Currency {
-  final String currencyName;
-  final String imageFile;
-  final String rateInUSD;
-  final String rateInINR;
-
-  Currency(this.currencyName, this.imageFile, this.rateInUSD, this.rateInINR);
-}
--- lib/subscribed_data.dart
+++ lib/subscribed_data.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:demo_crypto_app/currency.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
