732c3ffde44fc165f6c53ac597890579bc3af4ec
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2021-11-16 01:02:59 +0530

    comment out some part of the code

--- lib/services/crypto_data.dart
+++ lib/services/crypto_data.dart
@@ -25,12 +25,12 @@ class CryptoData {
   List<int> ratesList = [];
 
   Future<void> getCryptoData() async {
-    for (int index = 0; index < cryptoCurrenciesList.length; index++) {
-      NetworkHelper networkHelper =
-          NetworkHelper('$coinCapURL/${cryptoCurrenciesList[index]}');
-      var currencyData = await networkHelper.getData();
-      var currentRateInString = currencyData['data']['rateUsd'];
-      ratesList.add(double.parse(currentRateInString).toInt());
+    // for (int index = 0; index < cryptoCurrenciesList.length; index++) {
+    //   NetworkHelper networkHelper =
+    //       NetworkHelper('$coinCapURL/${cryptoCurrenciesList[index]}');
+    //   var currencyData = await networkHelper.getData();
+    //   var currentRateInString = currencyData['data']['rateUsd'];
+    //   ratesList.add(double.parse(currentRateInString).toInt());
     }
 
     // void updateSubscribedCrypto(ReusableCard currency) {
